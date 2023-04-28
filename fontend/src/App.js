415855ebe58f095dd62e5dd452087084bc2ad8a8
akhilsingh2003 <akhilsinghrajpoot1@gmail.com>
2023-04-28 10:29:11 +0530

    frontend part Completed

--- fontend/src/App.js
+++ fontend/src/App.js
@@ -1,12 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.scss";
+import Home from "./pages/home/Home";
+import Register from "./pages/register/Register";
+import Watch from "./pages/watch/Watch";
+import Login from "./pages/login/Login";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
+import { useContext } from "react";
+import { AuthContext } from "./authContext/AuthContext";
 
-function App() {
+const App = () => {
+  const { user } = useContext(AuthContext);
   return (
-    <div className="App">
-     Hello World
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          {user ? <Home /> : <Redirect to="/register" />}
+        </Route>
+        <Route path="/register">
+          {!user ? <Register /> : <Redirect to="/" />}
+        </Route>
+        <Route path="/login">{!user ? <Login /> : <Redirect to="/" />}</Route>
+        {user && (
+          <>
+            <Route path="/movies">
+              <Home type="movie" />
+            </Route>
+            <Route path="/series">
+              <Home type="series" />
+            </Route>
+            <Route path="/watch">
+              <Watch />
+            </Route>
+          </>
+        )}
+      </Switch>
+    </Router>
   );
-}
+};
 
 export default App;
---
+++ fontend/src/pages/watch/Watch.jsx
@@ -0,0 +1,20 @@
+import { ArrowBackOutlined } from "@material-ui/icons";
+import { Link, useLocation } from "react-router-dom";
+import vid from "../../vedio/example.mp4"
+import "./watch.scss";
+
+export default function Watch() {
+  const location = useLocation();
+  const movie = location.movie;
+  return (
+    <div className="watch">
+      <Link to="/">
+        <div className="back">
+          <ArrowBackOutlined />
+          Home
+        </div>
+      </Link>
+      <video className="video" autoPlay progress controls src={vid} />
+    </div>
+  );
+}
